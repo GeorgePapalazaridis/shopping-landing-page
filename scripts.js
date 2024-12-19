0ddf62553947bf72a6dfcc3f05f2e955da79c433
George Papalazaridis <georgepapalazaridis@gmail.com>
2024-12-19 23:19:48 +0200

    Add script file and refine navigation styles for improved layout and usability

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// "use strict";
+
+// document.getElementById("menu-toggle").addEventListener("click", function () {
+//     const menu = document.getElementById("menu-list");
+//     menu.classList.toggle("active");
+// });
